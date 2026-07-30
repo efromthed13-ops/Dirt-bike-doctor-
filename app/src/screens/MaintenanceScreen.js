21f7494c4af59be88677b7dacc6da6c416149f58
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:24:14 -0700

    Add maintenance screen foundation

---
+++ app/src/screens/MaintenanceScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function MaintenanceScreen() {
+  return {
+    title: 'Maintenance Tracker',
+    actions: [
+      'Add Service Record',
+      'View Upcoming Maintenance',
+      'View History'
+    ]
+  };
+}
