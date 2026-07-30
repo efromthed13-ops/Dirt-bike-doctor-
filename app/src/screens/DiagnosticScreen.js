e3a4f6bbd6cf0db8c83fa452bc95bb8ee909aef4
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:20:37 -0700

    Add diagnostic screen foundation

---
+++ app/src/screens/DiagnosticScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function DiagnosticScreen() {
+  return {
+    title: 'Diagnose My Bike',
+    steps: [
+      'Select symptom',
+      'Answer checks',
+      'View possible causes',
+      'Follow repair guide'
+    ]
+  };
+}
