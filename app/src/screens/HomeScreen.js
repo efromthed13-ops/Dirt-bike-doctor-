c177153b4b4e33ffe57d19e67c7ba2384485eed0
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:20:31 -0700

    Add first working home screen component

---
+++ app/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HomeScreen() {
+  return {
+    title: 'Dirt Bike Doctor',
+    buttons: [
+      'Diagnose My Bike',
+      'My Garage',
+      'Maintenance',
+      'Repair Guides'
+    ]
+  };
+}
