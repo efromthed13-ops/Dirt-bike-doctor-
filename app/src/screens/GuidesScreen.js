ab90da68147ae3089efa7d1b3e5d02cc54c93d3e
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:24:19 -0700

    Add repair guides screen foundation

---
+++ app/src/screens/GuidesScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function GuidesScreen() {
+  return {
+    title: 'Repair Library',
+    categories: [
+      'Engine',
+      'Electrical',
+      'Fuel',
+      'Cooling',
+      'Brakes',
+      'Suspension'
+    ]
+  };
+}
