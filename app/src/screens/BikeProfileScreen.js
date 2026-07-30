22e23813540d9313c5d2a7e33d539a8d8717425c
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:26:28 -0700

    Add bike profile screen foundation

---
+++ app/src/screens/BikeProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function BikeProfileScreen() {
+  return {
+    title: 'Bike Profile',
+    fields: [
+      'Brand',
+      'Model',
+      'Year',
+      'Engine Size',
+      'Hours'
+    ]
+  };
+}
