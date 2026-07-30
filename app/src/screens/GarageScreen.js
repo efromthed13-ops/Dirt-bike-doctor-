950e0b03ac07c5d54bcdc27feeba91712e4dc0eb
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:24:09 -0700

    Add garage screen foundation

---
+++ app/src/screens/GarageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function GarageScreen() {
+  return {
+    title: 'My Garage',
+    actions: [
+      'Add Bike',
+      'View Service History',
+      'View Repair Notes'
+    ]
+  };
+}
