252707fb357a388de7b5d75518f4c4381ae972d7
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:21:41 -0700

    Add symptom search component foundation

---
+++ app/src/components/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function SearchBox() {
+  return {
+    placeholder: 'Describe your bike problem',
+    examples: [
+      'won\'t start',
+      'has spark but no run',
+      'overheating'
+    ]
+  };
+}
