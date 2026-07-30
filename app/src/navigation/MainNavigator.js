fd8a6e1820e7d2a3967ea92e4bfd4476926ef9e4
efromthed13-ops <e.from.the.d13@gmail.com>
2026-07-29 19:24:54 -0700

    Add main app navigation foundation

---
+++ app/src/navigation/MainNavigator.js
@@ -0,0 +1,10 @@
+import { routes } from './routes';
+
+export const mainNavigator = [
+  routes.HOME,
+  routes.DIAGNOSE,
+  routes.RESULTS,
+  routes.GARAGE,
+  routes.MAINTENANCE,
+  routes.GUIDES
+];
